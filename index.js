d0e1ff551b6af9190023aa2c6ce6013d7a3b7036
MUINDI23 <bbamnrb203915@spu.ac.ke>
2025-03-20 11:30:56 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+const nameArray = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "suprise";
+console.log(writeCards(nameArray, eventName));
+
+function countDown (countNum) {
+    while (countNum >= 0) {
+        console.log(countNum);
+        countNum--;
+    }
+}
+
+countDown(10);
